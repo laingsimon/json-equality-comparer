12ea0fb84fd478ded6ced60eac480dda55e527c8
Simon Laing <laingsimon@users.noreply.github.com>
2020-12-22 07:56:38 +0000

    Add Comparer.cs, a synonym of JsonEqualityComparer
    To remove the need to fully qualify the class (with the namespace) when using the comparer.

---
+++ JsonEqualityComparer/Comparer.cs
@@ -0,0 +1,9 @@
+namespace JsonEqualityComparer
+{
+    /// <summary>
+    /// Compare two JSON objects and report the differences
+    /// This is a synonym of the JsonEqualityComparer
+    /// </summary>
+    public class Comparer : JsonEqualityComparer
+    { }
+}
